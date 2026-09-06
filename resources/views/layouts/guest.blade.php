ee1bfa550587a1dfef513f7d55e28846f4fe877a
Myke <mykegabrielgenciane15@gmail.com>
2026-09-07 00:56:27 +0800

    11

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        @include('vendor.laravelpwa.meta')
+
         <title>{{ config('app.name', 'QR Attendance') }}</title>
 
         <!-- Scripts -->
--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,13 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/serviceworker.js', function() {
+    $config = config('laravelpwa');
+    return response()
+        ->view('laravelpwa::serviceworker', compact('config'))
+        ->header('Content-Type', 'application/javascript');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
