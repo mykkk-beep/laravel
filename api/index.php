86f46eb4066dd539ba926675644c7bb7ba66394e
Myke <mykegabrielgenciane15@gmail.com>
2026-09-19 15:41:36 +0800

    15th

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
 
-require __DIR__.'/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$request = Illuminate\Http\Request::capture();
+
+$response = $app->handleRequest($request);
+
+$response->send();
